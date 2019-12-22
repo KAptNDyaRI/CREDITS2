6497b4612de11e509c784f227f60304c15137c99
KAptNDyaRI <44928763+KAptNDyaRI@users.noreply.github.com>
2019-12-22 17:31:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("658036127437160458")
+let channel =     client.channels.get("")
 setInterval(function() {
 channel.send(`kaptnA2 kaptAn kaptn1A kaptnA1 kaptnA1`);
 }, 30)
