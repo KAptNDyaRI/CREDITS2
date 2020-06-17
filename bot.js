5b3c1bcce1051c014aa76b744d69586e1691c787
KAptNDyaRI <44928763+KAptNDyaRI@users.noreply.github.com>
2020-06-17 23:30:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,11 +3,39 @@ const client = new Discord.Client();
 console.log("Scrpit By Dream");
 
 
-client.on("ready", () => {
-let channel =     client.channels.get("617451409608146944")
-setInterval(function() {
-channel.send(`لا تبجي لا تبجي لا تبجي لا تبجي`);
-}, 30)
-})
+client.on("message", message => {
+  if (message.author.bot) return;
 
-client.login(process.env.BOT_TOKEN);
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  // +say
+  if (command === "say") {
+    if (!message.channel.guild)
+      return message.channel
+        .send("اسف هاذا امر خاص لسرفر")
+        .then(m => m.delete(5000));
+    if (!message.member.hasPermission("ADMINISTRATOR"))
+      return message.channel.send("ما عندك صلاحيات ADMINISTRATOR");
+    message.delete();
+    message.channel.sendMessage(args.join(" "));
+  } 
+
+  if (command == "embed") {
+    if (!message.channel.guild)
+      return message.channel
+        .send("اسف هاذا امر خاص لسرفر")
+        .then(m => m.delete(5000));
+    if (!message.member.hasPermission("MANAGE_MESSAGES"))
+      return message.channel.send("ما عندك صلاحيات MANAGE_MESSAGES");
+    let say = new Discord.RichEmbed()
+      .setDescription(args.join("  "))
+      .setColor(0x23b2d6);
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+});
